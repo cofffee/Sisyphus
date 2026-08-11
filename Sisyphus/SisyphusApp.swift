e38603cb2e69c406b9df80041abe16141798177b
Kevin <remigiokevin@gmail.com>
2026-08-11 11:38:36 -0400

    Initial Commit

---
+++ Sisyphus/SisyphusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SisyphusApp.swift
+//  Sisyphus
+//
+//  Created by Kevin Remigio on 8/11/26.
+//
+
+import SwiftUI
+
+@main
+struct SisyphusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SisyphusTests/SisyphusTests.swift
@@ -0,0 +1,17 @@
+//
+//  SisyphusTests.swift
+//  SisyphusTests
+//
+//  Created by Kevin Remigio on 8/11/26.
+//
+
+import Testing
+@testable import Sisyphus
+
+struct SisyphusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
